43083c5f1f1bd327b684006b530b7624da796c5a
Daniel Silva <>
2023-11-27 20:47:28 -0500

    aula-20231127

--- overview/scripts.js
+++ overview/scripts.js
@@ -413,7 +413,7 @@ tenis.name = 'tenis Jordan'
 // através do objeto nativo document, é possível executar métodos que permitem obter e manipular o HTML
 
 // seleciona elemento pelo id; pouco recomendado fazer dessa forma
-console.log(document.getElementById('titulo'))
+console.log(document.getElementById('title'))
 
 // seleciona vários elementos pelo nome da classe e cria um array
 const texts = document.querySelectorAll('.text')
@@ -447,4 +447,4 @@ console.log('quando isso aparece?')
 
 // Desafio 07: utilize a classe nativa Date para, dentro de uma função, exibir via console uma string com o dia, mês e ano atual. Em outras palavras: crie uma função sem parâmetros, crie um objeto do tipo Date dentro dela e utilize os métodos getHours, getMinutes e getSeconds para montar uma string com a hora, minuto e segundo. Após a função, acrescente o código setTimeout(nomeDaFuncao, 5000) três vezes. Teste com o comando node ./nomedoarquivo.js via terminal. Perguntas retóricas: o que acontece? Qual o motivo disso acontecer? Agora, utilize o método setInterval apenas uma vez no lugar de setTimeout e responda às mesmas perguntas.
 
-// Desafio 08: Crie um documento HTML com apenas um elemento ul, ou seja, uma lista não ordenada. Em seguida, crie um documento em JavaScript, utilize fetch (é uma API nativa do ES6 para requisições HTTP através de Promises) para pegar o conteúdo da página http://jsonplaceholder.typicode.com/users. Em seguida, dentro do fetch, utilize um then para converter o conteúdo da página para um objeto JSON e outro then para mapear o vetor com os dados do objeto JSON, puxando o nome e email de cada dado para a lista no HTML. Trate também o erro, quando houver. Inicie suas pesquisas para resolver o problema em https://www.devmedia.com.br/javascript-fetch/41206. Métodos do objeto document que podem ser úteis: createElement, textContent, innerHTML, appendChild, querySelector.Métodos de vetor que podem ser úteis: forEach, map
+// Desafio 08: Crie um documento HTML com apenas um elemento ul, ou seja, uma lista não ordenada. Em seguida, crie um documento em JavaScript, utilize fetch (é uma API nativa do ES6 para requisições HTTP através de Promises) para pegar o conteúdo da página http://jsonplaceholder.typicode.com/users. Em seguida, dentro do fetch, utilize um then para converter o conteúdo da página para um objeto JSON e outro then para mapear o vetor com os dados do objeto JSON, puxando o nome e email de cada dado para a lista no HTML. Trate também o erro, quando houver. Inicie suas pesquisas para resolver o problema em https://www.devmedia.com.br/javascript-fetch/41206. Métodos e atributos do objeto document que podem ser úteis: createElement, textContent, innerHTML, appendChild, querySelector. Métodos de vetor que podem ser úteis: forEach, map
