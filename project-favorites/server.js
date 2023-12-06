ad6ccc9fffe606da6b1cce166a70aee18c0a09d6
vrcvieira <victor.vieira@ifac.edu.br>
2023-12-05 20:35:32 -0500

    aula-20231205

--- project-favorites/server.js
+++ project-favorites/server.js
@@ -8,7 +8,7 @@ http.createServer((req, res) => {
     const filePath = path.join(__dirname,'public',file)
 
     const extname = path.extname(filePath)
-    const allowedFilesTypes = [ '.html', '.css', '.js' ]
+    const allowedFilesTypes = [ '.html', '.css', '.js', '.jpg' ]
     const allowed = allowedFilesTypes.find( item => item == extname )
     if(!allowed) return
 
